269b4221db7fcac8f9a4afd6081245f1150f8abf
Alin Khatri <alin.khatri@tekkon.com.np>
2023-02-08 00:39:38 +0545

    Updated user flow to verify password first

--- routers/User.js
+++ routers/User.js
@@ -5,7 +5,7 @@ import { isAuthenticated } from "../middleware/auth.js";
 const router = express.Router();
 
 router.route("/register").post(register);
-router.route("/verify").post(isAuthenticated, verify);
+router.route("/verify").post(verify);
 
 router.route("/login").post(login);
 router.route("/me").get(isAuthenticated, getMyProfile);
